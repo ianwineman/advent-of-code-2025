5a5b334e02cedea832230c81b6e284bcf4898d98
Ian Wineman <130872935+ianwineman@users.noreply.github.com>
2025-12-03 06:53:49 -0500

    complete day 3 part 1

---
+++ day3_part1.jl
@@ -0,0 +1,23 @@
+input = "987654321111111
+811111111111119
+234234234234278
+818181911112111"
+
+function parse_input(input::String)::Vector{Vector{Int64}}
+	lines = split(input)
+	banks = Vector{Vector{Int64}}(undef, length(lines))
+	for (i,bank) in enumerate(lines)
+		banks[i] = parse.(Int, split(bank, ""))
+	end
+	return banks
+end
+
+function maxiumum_joltage(bank::Vector{Int64})
+	x, i = findmax(bank[1:end-1])
+	y, _ = findmax(bank[i+1:end])
+	return parse(Int, string(x) * string(y))
+end
+
+banks = parse_input(input)
+joltages = [maxiumum_joltage(b) for b in banks]
+sum(joltages)
