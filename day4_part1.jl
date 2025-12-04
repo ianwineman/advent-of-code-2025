1f1e7a857c8361525dbab42614d9c0b6fd8b70d5
Ian Wineman <130872935+ianwineman@users.noreply.github.com>
2025-12-04 08:49:03 -0500

    complete day 4 part 1

---
+++ day4_part1.jl
@@ -0,0 +1,43 @@
+input = "..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@."
+
+function parse_input(input::String)
+	lines = split(input)
+	grid = zeros(Int, length(lines)+2,length(lines[1])+2)
+	for (i,line) in enumerate(lines)
+		for (j,cell) in enumerate(line)
+			if string(cell) == "@"
+				grid[i+1, j+1] = 1
+			end
+		end
+	end
+	return grid
+end
+
+function convolution(A)
+	K = ones(Int, 3, 3)
+	K[2,2] = 0
+	return sum(A .* K)
+end
+
+grid = parse_input(input)
+movable = 0
+
+for i in 2:size(grid)[1]-1
+	for j in 2:size(grid)[2]-1
+		A = grid[i-1:i+1, j-1:j+1]
+		c = convolution(A)
+		if (c < 4) && (A[2, 2] == 1)
+			global movable += 1
+		end
+	end
+end
+movable
